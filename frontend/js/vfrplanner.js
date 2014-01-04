afa34cbcb13f150a774fda294a96505b5b4726ab
Sagar Behere <sagar@sagar.se>
2014-01-04 02:16:31 +0100

    added dgrid table

--- frontend/js/vfrplanner.js
+++ frontend/js/vfrplanner.js
@@ -7,6 +7,41 @@
 
 var app = angular.module('VFRPlanner', ['leaflet-directive']);
 
+require(["dgrid/OnDemandGrid", "dgrid/extensions/CompoundColumns", "dgrid/Selection", "dgrid/Keyboard", "dojo/_base/declare", "dojo/_base/lang", "dojo/domReady!"], function(Grid, CompoundColumns, Selection, Keyboard, declare, lang){
+    var CompoundedGrid = declare([Grid, CompoundColumns, Selection, Keyboard]);
+    var navColumns = [
+        {label: "Wind", children: [
+                {field: 'wd', label: 'Dir.', sortable: false},
+                {field: 'ws', label: 'Speed', sortable: false}
+        ]},
+        {field: 'alt', label: 'ALT', sortable: false},
+        {field: 'tt', label: 'TT', sortable: false},
+        {field: 'tas', label: 'TAS', sortable: false},
+        {field: 'wca', label: 'wca', sortable: false},
+        {field: 'th', label: 'TH', sortable: false},
+        {field: 'var', label: 'var', sortable: false},
+        {field: 'mh', label: 'MH', sortable: false},
+        {field: 'dev', label: 'dev', sortable: false},
+        {field: 'ch', label: 'CH', sortable: false},
+        {field: 'waypt', label: 'Waypoint', sortable: false},
+        {label: 'Distance', children: [
+                {field: 'dleg', label: 'leg', sortable: false},
+                {field: 'dacc', label: 'acc', sortable: false}
+        ]},
+        {field: 'gs', label: 'GS', sortable: false},
+        {label: 'Time', children: [
+                {field: 'tint', label: 'int', sortable: false},
+                {field: 'tacc', label: 'acc', sortable: false},
+                {field: 'teto', label: 'ETO', sortable: false}
+        ]}
+    ];
+    window.grid = new CompoundedGrid({
+        columns: navColumns
+    }, "navgrid"
+            );
+}
+        
+        );
 app.controller("mapController", [ '$scope', 'leafletEvents', function($scope, leafletEvents){
         $scope.navpointText = "No point clicked yet.";
         $scope.center = {
